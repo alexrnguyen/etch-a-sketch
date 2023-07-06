501fe1599181ee1938b2ea7bb1e2f60ae0d0532d
alexrnguyen <alexander.nguyen@shaw.ca>
2023-07-06 17:20:00 -0600

    Create grid of squares

---
+++ etch-a-sketch.js
@@ -0,0 +1,70 @@
+const GRID_SIDE_LENGTH = 500;
+
+function createGrid(gridSize) {
+    const grid = document.querySelector(".grid-container");
+    // Create an n x n grid (where n is gridSize)
+    for(let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div')
+        row.classList.add('row')
+        for(let j = 0; j < gridSize; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square')
+            square.style.backgroundColor = 'white';
+            const sideLength = GRID_SIDE_LENGTH/gridSize;
+            square.style.width = `${sideLength}px`;
+            square.style.height = `${sideLength}px`;
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
+    }
+}
+
+function draw() {
+    const grid = document.querySelector(".grid-container");
+    
+}
+
+function randomizeColour() {
+
+}
+
+function erase() {
+
+}
+
+function clearGrid() {
+
+}
+
+function main() {
+    
+    // Buttons
+    const colourPicker = document.querySelector(".colour-picker");
+    const rainbowButton = document.querySelector(".rainbow-mode");
+    const eraser = document.querySelector(".eraser");
+    const clear = document.querySelector(".clear");
+
+    const grid = document.querySelector(".grid-container");
+    let gridSize = 100;
+
+    createGrid(gridSize);
+
+    colourPicker.addEventListener('click', () => {
+        console.log('Colour picker clicked!');
+    });
+
+    rainbowButton.addEventListener('click', () => {
+        console.log('Rainbow mode clicked!');
+    });
+
+    eraser.addEventListener('click', () => {
+        console.log('Eraser clicked!');
+    });
+
+    clear.addEventListener('click', () => {
+        console.log('Clear clicked!');
+    });
+    grid.addEventListener('mouseover', draw);
+}
+
+main();
