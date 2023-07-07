e57d1041c21889ab44e6fab747a97bda02954ce2
alexrnguyen <alexander.nguyen@shaw.ca>
2023-07-06 21:44:02 -0600

    Add grid resizing functionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,13 +1,17 @@
 const GRID_SIDE_LENGTH = 500;
 const MAX_SQUARES_IN_ROW = 100;
-
+const DEFAULT_SIZE = 16;
+/**
+ * 
+ * @param {*} gridSize 
+ */
 function createGrid(gridSize) {
     const grid = document.querySelector(".grid-container");
     // Create an n x n grid (where n is gridSize)
-    for(let i = 0; i < gridSize; i++) {
+    for (let i = 0; i < gridSize; i++) {
         const row = document.createElement('div')
         row.classList.add('row')
-        for(let j = 0; j < gridSize; j++) {
+        for (let j = 0; j < gridSize; j++) {
             const square = document.createElement('div');
             square.classList.add('square')
             square.style.backgroundColor = 'white';
@@ -20,6 +24,27 @@ function createGrid(gridSize) {
     }
 }
 
+function removeSquares() {
+    const grid = document.querySelector(".grid-container");
+    while(grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+}
+
+function resizeGrid(gridSize) {
+    clearGrid();
+    removeSquares();
+    createGrid(gridSize);
+    return document.querySelectorAll('.square');
+}
+
+/**
+ * 
+ * @param {*} square 
+ * @param {*} colour 
+ * @param {*} eraserSelected 
+ * @param {*} rainbowSelected 
+ */
 function changeSquare(square, colour, eraserSelected, rainbowSelected) {
     if (eraserSelected) {
         square.style.backgroundColor = 'white';
@@ -32,6 +57,10 @@ function changeSquare(square, colour, eraserSelected, rainbowSelected) {
     }
 }
 
+/**
+ * 
+ * @param {*} square 
+ */
 function randomizeColour(square) {
     const redValue = Math.floor(Math.random() * 255);
     const greenValue = Math.floor(Math.random() * 255);
@@ -39,11 +68,19 @@ function randomizeColour(square) {
     square.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
 }
 
+/**
+ * 
+ */
 function clearGrid() {
     const squares = document.querySelectorAll('.square');
     squares.forEach(square => square.style.backgroundColor = 'white');
 }
 
+/**
+ * 
+ * @param {*} button 
+ * @param {*} selected 
+ */
 function changeButtonBackground(button, selected) {
     if (selected) {
         button.style.backgroundColor = 'yellow';
@@ -62,15 +99,23 @@ function main() {
     const eraser = document.querySelector(".eraser");
     const clear = document.querySelector(".clear");
 
+    const sizeHeader = document.querySelector(".slider-header");
+    const sizeSlider = document.querySelector(".slider");
+
+    let gridSize = DEFAULT_SIZE;
+    sizeSlider.value = gridSize;
+    sizeHeader.textContent = `${gridSize}x${gridSize}`;
+
+
     const grid = document.querySelector(".grid-container");
-    let gridSize = 16;
 
     createGrid(gridSize);
 
-    const squares = document.querySelectorAll('.square');
+    let squares = document.querySelectorAll('.square');
     let eraserSelected = false;
     let rainbowSelected = false;
-    
+
+    // Event Listeners
     squares.forEach(square => {
         square.addEventListener('mouseover', () =>{
             changeSquare(square, colour, eraserSelected, rainbowSelected)
@@ -93,6 +138,20 @@ function main() {
     });
 
     clear.addEventListener('click', clearGrid);
+
+    sizeSlider.oninput = e => {
+        gridSize = e.target.value;
+        sizeHeader.textContent = `${gridSize}x${gridSize}`;
+    }
+
+    sizeSlider.onchange = e => {
+        squares = resizeGrid(gridSize);
+        squares.forEach(square => {
+            square.addEventListener('mouseover', () =>{
+                changeSquare(square, colour, eraserSelected, rainbowSelected)
+            });
+        });
+    }
 }
 
 main();
